b3fa3731a936b5cb0426723d076ba53c9efc74e5
anastafish <anastafish24@gmail.com>
2023-05-09 19:11:40 +0300

    Create a Model

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 const env = require('dotenv').config();
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(process.env.MONGO_URI)
+const personSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  age:Number,
+  favoriteFoods:String
+})
 
 let Person;
+Person = mongoose.model('Person', personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
